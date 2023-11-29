66825f2a7223736fae5ea75c5b03b0246c66a61f
NatalieShuklin <natalieshuklin@gmail.com>
2023-11-29 22:57:43 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 export const PORT = 5555;
 
-export const mongoDBurl  ='mongodb+srv://natalie:nana@cluster0.nv4781a.mongodb.net/books-collection?retryWrites=true&w=majority';
+export const mongoDBurl  ='mongodb+srv://---a@cluster0.nv4781a.mongodb.net/books-collection?retryWrites=true&w=majority';
 
